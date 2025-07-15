4d31ddef54da2630931b900d78fce7254c3572b6
GabrielMagnusVieira <magnus181098@gmail.com>
2025-07-15 18:43:00 -0300

    3

--- test/specs/login.e2e.js
+++ test/specs/login.e2e.js
@@ -1,7 +1,13 @@
 import { expect, driver } from '@wdio/globals'
+import homePage from '../pageobjects/home.page.js'
+import loginPage from '../pageobjects/login.page.js'
+import profilePage from '../pageobjects/profile.page.js'
 
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
+        await homePage.openMenu('profile')
+        await loginPage.login('magnus181098@gmail.com', 'dilolilo')
+        expect ((await profilePage.profileName('magnus gabriel')).isDisplayed()).toBeTruthy()
         await driver.pause(10000)
     })
 })
